7e7301f85bcb93765e82e7320078307245c5957f
jcvetinovic <jelena.cvetinovic@traceone.com>
2024-10-18 10:21:03 +0200

    Implemented CreateMovie() and CreateProgram() methods
    Rebased branch with main

--- MovieFestival/App.cs
+++ MovieFestival/App.cs
@@ -6,5 +6,34 @@ class App
     {
         Console.WriteLine("Hi from App");
     }
+
+    public string CreateProgram()
+    {
+        string date = ReadUserInput("Insert program date");
+
+        return "new Program(date);";
+    }
+
+    public string CreateMovie()
+    {
+        string title = ReadUserInput("Insert movie title");
+        string length = ReadUserInput("Insert movie length");
+        string genre = ReadUserInput("Insert movie genre");
+
+        return "new Movie(title, length, genre);";
+    }
+
+    private string ReadUserInput(string message)
+    {
+        string? userInput = null;
+
+        while (string.IsNullOrEmpty(userInput))
+        {
+            Console.WriteLine(message);
+            userInput = Console.ReadLine();
+        }
+
+        return userInput;
+    }
 }
 
