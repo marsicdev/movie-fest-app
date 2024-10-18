db68af4a3f63d3b7c1bf82973aec92dfc89ff225
Jeckaa <48253691+Jeckaa@users.noreply.github.com>
2024-10-18 11:28:19 +0200

    Implemented CreateMovie() and CreateProgram() methods (#22)
    * Implemented CreateMovie() and CreateProgram() methods
    
    Rebased branch with main
    
    * Update CreateProgram and CreateMovie methods
    
    ---------
    
    Co-authored-by: jcvetinovic <jelena.cvetinovic@traceone.com>

--- MovieFestival/App.cs
+++ MovieFestival/App.cs
@@ -6,5 +6,39 @@ class App
     {
         Console.WriteLine("Hi from App");
     }
+
+    public Program CreateProgram()
+    {
+        string date = ReadUserInput("Insert program date");
+        Program program = new Program
+        {
+            Date = DateTime.Parse(date)
+        };
+
+        return program;
+    }
+
+    public Movie CreateMovie()
+    {
+        string title = ReadUserInput("Insert movie title");
+        string length = ReadUserInput("Insert movie length");
+        string genreStr = ReadUserInput("Insert movie genre");
+        Genre genre = new Genre(genreStr);
+
+        return new Movie(title, int.Parse(length), genre);
+    }
+
+    private string ReadUserInput(string message)
+    {
+        string? userInput = null;
+
+        while (string.IsNullOrEmpty(userInput))
+        {
+            Console.WriteLine(message);
+            userInput = Console.ReadLine();
+        }
+
+        return userInput;
+    }
 }
 
