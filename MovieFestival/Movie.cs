52345422502acf122e1b8e489009aa19f2db47cf
Magdalena Stankovic <magdalena.stankovic@traceone.com>
2024-10-18 10:41:15 +0200

    Update Movie class with Genre class

--- MovieFestival/Movie.cs
+++ MovieFestival/Movie.cs
@@ -10,9 +10,9 @@ namespace MovieFestival
 	{
 		public string Title { get; set; }
 		public int Length { get; set; }
-		public string Genre { get; set; }
+		public Genre Genre { get; set; }
 
-		public Movie(string title, int length, string genre)
+		public Movie(string title, int length, Genre genre)
 		{
 			Title = title;
 			Length = length;
@@ -21,9 +21,7 @@ namespace MovieFestival
 
 		public string GetData()
 		{
-			string acronym = !string.IsNullOrEmpty(Genre) ? $"{Genre[0]}{Genre[^1]}" : "";
-
-			return string.Format("Title: {0}, Length: {1}, Genre {2}", Title, Length, acronym);
+			return string.Format("Title: {0}, Length: {1}, Genre {2}", Title, Length, Genre.GetData());
 		}
 	}
 }
