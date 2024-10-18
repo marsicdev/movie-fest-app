6f7154b8cf4fb978518748524274b6644d49874e
stefandobrodolac <stefan.dobrodolac@traceone.com>
2024-10-18 10:14:11 +0200

    Add Genre class

---
+++ MovieFestival/Genre.cs
@@ -0,0 +1,20 @@
+﻿namespace MovieFestival
+{
+    public class Genre
+    {
+        public string Name { get; set; }
+
+        public Genre(string name)
+        {
+            Name = name;
+        }
+
+        public string GetData()
+        {
+            if (!string.IsNullOrEmpty(Name))
+                return $"{Name[0]}{Name[Name.Length - 1]}";
+            else
+                return string.Empty;
+        }
+    }
+}
